9dbd6b1c5cbab0c37da38626045059fe206df660
AdryanneKelly <adryannekelly123@gmail.com>
2023-04-10 11:12:19 -0300

    Blog do curso de PHP

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+
+include_once("helpers/url.php");
+
+?>
+
+
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog Codar</title>
+  <!-- ESTILOS DO PROJETO --->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+  <!-- GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <h1>Blog Codar</h1>
+</body>
+
+</html>
